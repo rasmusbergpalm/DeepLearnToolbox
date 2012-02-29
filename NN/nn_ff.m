459d3bda27c1888ef871b92dc03aff9cfd20ed66
Carl Cotner <carl.cotner@gmail.com>
2012-02-29 00:14:37 -0500

    Formatted nn_*.m files.

--- NN/nn_ff.m
+++ NN/nn_ff.m
@@ -1,13 +1,16 @@
-function net = nnff(net, x, y)
-    n = numel(net.size);
-    m = size(x,1);
+function net = nn_ff(net, x, y)
+%    n = numel(net.size);
+    n = net.n;
+    m = size(x, 1);
+
     net.a{1} = x;
 
-    %% feedforward pass
-    for i=2:n
-        net.a{i} = sigm(repmat(net.b{i-1}',m,1) + net.a{i-1}*net.W{i-1}' + net.eta*randn(m,numel(net.b{i-1})));
-        net.p{i} = 0.99*net.p{i} + 0.01*sum(net.a{i},1)/size(net.a{i},1);
+    %%  feedforward pass
+    for i = 2 : n
+        net.a{i} = sigm(repmat(net.b{i - 1}', m, 1) + net.a{i - 1} * net.W{i - 1}' + net.eta * randn(m, numel(net.b{i - 1})));
+        net.p{i} = 0.99 * net.p{i} + 0.01 * sum(net.a{i}, 1) / size(net.a{i}, 1);
     end
-    net.e = y-net.a{n};
-    net.L = 1/2*sum(sum(net.e.^2))/m; 
+
+    net.e = y - net.a{n};
+    net.L = 0.5 * sum(sum(net.e .^ 2)) / m; 
 end
--- NN/nn_train.m
+++ NN/nn_train.m
@@ -1,39 +1,53 @@
-function net = nntrain(net, x, y, opts)
-    m = size(x,1);
-    numbatches = m/opts.batchsize;
-    if(rem(numbatches,1)~=0)
-        error('numbatches not integer');
-    end
-    net.rL = [];
-    n=1;
-    for i=1:opts.numepochs
-        kk = randperm(m);
-        tic;
-        for l=1:numbatches
-            batch_x = x(kk((l-1)*opts.batchsize+1:l*opts.batchsize),:);
-            if(strcmp(class(batch_x),'uint8'))
-                batch_x = double(batch_x)/255;
-            end
-            batch_y = y(kk((l-1)*opts.batchsize+1:l*opts.batchsize),:);
-            if(strcmp(class(batch_y),'uint8'))
-                batch_y = double(batch_y)/255;
-            end
-            net = nnff(net, batch_x, batch_y);
-    %             if(rand() < 1e-3)
-    %                 disp 'Performing numerical gradient checking ...';
-    %                 nnchecknumgrad(net, x(i,:), y(i,:));
-    %                 disp 'No errors found ...';
-    %             end
-            net = nnbp(net);
-            net = nnapplygrads(net);
-            if(isempty(net.rL))
-                net.rL(n) = net.L;
-            end
-            net.rL(n+1) = 0.99*net.rL(n) + 0.01*net.L;
-            n=n+1;
-        end
-        toc;
-        disp(['epoch ' num2str(i) '/' num2str(opts.numepochs)]);
+function net = nn_train(net, x, y, opts)
+    m = size(x, 1);
+
+    if strcmp(class(x), 'uint8')
+        x = double(x) / 255;
     end
 
+    if strcmp(class(y), 'uint8')
+        y = double(y) / 255;
+    end
+
+    batchsize = opts.batchsize;
+    numepochs = opts.numepochs
+
+    numbatches = m / batchsize;
+
+    if rem(numbatches, 1) ~= 0
+        error('numbatches not integer');
+    end
+
+%    net.rL = [];
+    net.rL = zeros(1, numepochs * numbatches);
+    n = 1;
+    for i = 1 : numepochs
+        tic;
+
+        kk = randperm(m);
+        for l = 1 : numbatches
+            batch_x = x(kk((l - 1) * batchsize + 1 : l * batchsize), :);
+            batch_y = y(kk((l - 1) * batchsize + 1 : l * batchsize), :);
+
+            net = nn_ff(net, batch_x, batch_y);
+%                if rand() < 1e-3
+%                    disp 'Performing numerical gradient checking ...';
+%                    nn_checknumgrad(net, x(i, :), y(i, :));
+%                    disp 'No errors found ...';
+%                end
+            net = nn_bp(net);
+            net = nn_applygrads(net);
+
+%            if isempty(net.rL)
+            if n == 1
+                net.rL(n) = net.L;
+            end
+
+            net.rL(n + 1) = 0.99 * net.rL(n) + 0.01 * net.L;
+            n = n + 1;
+        end
+
+        toc;
+        disp(['epoch ' num2str(i) '/' num2str(numepochs)]);
+    end
 end
