d3dd89278876888c8e681390ce2696c419b1370d
Carl Cotner <carl.cotner@gmail.com>
2012-02-29 00:39:43 -0500

    Finished formatting the remaining files.

--- NN/nn_test.m
+++ NN/nn_test.m
@@ -1,14 +1,14 @@
-function [er, bad] = nntest(net, x, y)
+function [er, bad] = nn_test(net, x, y)
     e = 0;
     bad = [];
-    for i=1:size(x,1)
-        %feedforward
-        net = nnff(net, x(i,:), y(i,:));
-        [~,g]=max(net.a{net.n});
-        if(g ~= find(y(i,:)))
-            e = e+1;
+    for i = 1 : size(x, 1)
+        %  feedforward
+        net = nn_ff(net, x(i, :), y(i, :));
+        [~, g] = max(net.a{net.n});
+        if g ~= find(y(i, :))
+            e = e + 1;
             bad = [bad; i];
         end
     end
-    er = e/size(x,1);
+    er = e / size(x, 1);
 end
