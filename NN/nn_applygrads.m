da33bd9cd431a08e7ed56d6f1192b54dd43fa985
Carl Cotner <carl.cotner@gmail.com>
2012-03-01 19:26:46 -0500

    Removed nn_* files.

--- NN/nn_applygrads.m
+++
@@ -1,7 +0,0 @@
-function net = nn_applygrads(net)
-%    for i = 1 : (numel(net.size) - 1)
-    for i = 1 : (net.n - 1)
-        net.W{i} = net.W{i} - net.alpha * (net.dW{i} + net.lambda * net.W{i});
-        net.b{i} = net.b{i} - net.alpha * net.db{i};
-    end
-end
