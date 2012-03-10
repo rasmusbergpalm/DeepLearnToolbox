97c9c0ccf78e807471d9c4d4b8c09235001adb0c
Carl Cotner <carl.cotner@gmail.com>
2012-03-09 23:27:14 -0500

    Minor reformatting.

--- SAE/saetrain.m
+++ SAE/saetrain.m
@@ -1,8 +1,11 @@
 function sae = saetrain(sae, x, opts)
-    for i = 1 : numel(sae.ae);
-%        disp(['Training AE ' num2str(i) '/' num2str(numel(sae.ae))]);
-        printf('Training AE %d / %d.', num2str(i), num2str(numel(sae.ae)));
+    n = numel(sae.ae);
+
+    for i = 1 : n
+        printf('Training AE %d / %d.', i, n);
+
         sae.ae{i} = nntrain(sae.ae{i}, x, x, opts);
+
         t = nnff(sae.ae{i}, x, x);
         x = t.a{2};
     end
