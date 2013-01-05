6328a83fae0d79064ac87fe48a878993548b85bd
Rasmus Berg Palm <rasmusbergpalm@gmail.com>
2013-01-05 14:29:11 +0100

    Added momentum

--- NN/nnapplygrads.m
+++ NN/nnapplygrads.m
@@ -2,10 +2,11 @@ function nn = nnapplygrads(nn)
 %NNAPPLYGRADS updates weights and biases with calculated gradients
 % nn = nnapplygrads(nn) returns an neural network structure with updated
 % weights and biases
-
-    %  TODO add momentum
+    
     for i = 1 : (nn.n - 1)
-        nn.W{i} = nn.W{i} - nn.learningRate * (nn.dW{i} + nn.weightPenaltyL2 * nn.W{i});
-        nn.b{i} = nn.b{i} - nn.learningRate * nn.db{i};
+        nn.vW{i} = nn.momentum*nn.vW{i} + nn.learningRate * (nn.dW{i} + nn.weightPenaltyL2 * nn.W{i});
+        nn.vb{i} = nn.momentum*nn.vb{i} + nn.learningRate * nn.db{i};
+        nn.W{i} = nn.W{i} - nn.vW{i};
+        nn.b{i} = nn.b{i} - nn.vb{i}; 
     end
 end
--- NN/nnsetup.m
+++ NN/nnsetup.m
@@ -1,12 +1,13 @@
-function nn = nnsetup(size)
+function nn = nnsetup(architecture)
 %NNSETUP creates a Feedforward Backpropagate Neural Network
 % nn = nnsetup(size) returns an neural network structure with n=numel(size)
 % layers, size being a n x 1 vector of layer sizes e.g. [784 100 10]
 
-    nn.size   = size;
+    nn.size   = architecture;
     nn.n      = numel(nn.size);
     
     nn.learningRate                     = 0.1;    %  learning rate 
+    nn.momentum                         = 0.5;    %  Momentum
     nn.weightPenaltyL2                  = 0;      %  L2 regularization
     nn.nonSparsityPenalty               = 0;      %  Non sparsity penalty
     nn.sparsityTarget                   = 0.05;   %  Sparsity target
@@ -15,9 +16,15 @@ function nn = nnsetup(size)
     nn.testing                          = 0;      %  Internal variable. nntest sets this to one.
 
     for i = 2 : nn.n
-        nn.b{i - 1} = zeros(nn.size(i), 1);   %  biases
-                                              %  weights
+        % biases and bias momentum
+        nn.b{i - 1} = zeros(nn.size(i), 1);
+        nn.vb{i - 1} = zeros(size(nn.b{i - 1}));
+        
+        % weights and weight momentum
         nn.W{i - 1} = (rand(nn.size(i), nn.size(i - 1)) - 0.5) * 2 * 4 * sqrt(6 / (nn.size(i) + nn.size(i - 1)));
-        nn.p{i}     = zeros(1, nn.size(i));   %  average activations (for use with sparsity)
+        nn.vW{i - 1} = zeros(size(nn.W{i - 1}));
+        
+        % average activations (for use with sparsity)
+        nn.p{i}     = zeros(1, nn.size(i));   
     end
 end
