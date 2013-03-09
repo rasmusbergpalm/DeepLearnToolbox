f6c01d26ad47eddfd9cee4cb9dc5f62b09709e25
Søren Sønderby <tropehjelm@gmail.com>
2013-03-09 14:08:54 +0100

    add nneval, nnupdatefigures and adds plotting functionality to nntrain
    nntrain changed to optonally take val_x and val_y as last to arguments.
    nntrain plots training progreess if opts.plot = 1
    
    nneval:Used for evaluating performance of network on training and
    optionally validation data
    
    nnupdatefigures: updates figures and plots the training progress.

--- NN/nntrain.m
+++ NN/nntrain.m
@@ -1,57 +1,91 @@
-function [nn, L] = nntrain(nn, x, y, opts)
+function [nn, L]  = nntrain(nn, train_x, train_y, opts, val_x, val_y)
 %NNTRAIN trains a neural net
-% [nn, L] = nnff(nn, x, y, opts) trains the neural network nn with input x and 
+% [nn, L] = nnff(nn, x, y, opts) trains the neural network nn with input x and
 % output y for opts.numepochs epochs, with minibatches of size
 % opts.batchsize. Returns a neural network nn with updated activations,
-% errors, weights and biases, (nn.a, nn.e, nn.W, nn.b) and L, the sum 
+% errors, weights and biases, (nn.a, nn.e, nn.W, nn.b) and L, the sum
 % squared error for each training minibatch.
 
-    assert(isfloat(x), 'x must be a float');
-    m = size(x, 1);
+assert(isfloat(train_x), 'train_x must be a float');
+assert(nargin == 4 || nargin == 6,'number ofinput arguments must be 4 or 6')
+
+loss.train.e               = [];
+loss.train.e_frac          = [];
+if nargin == 4 % training data
+    opts.validation = 0;
     
-    if nn.normalize_input==1
-        [x, mu, sigma] = zscore(x);
-        nn.normalizeMean = mu;
-        sigma(sigma==0) = 0.0001;%this should be very small value. 
-        nn.normalizeStd  = sigma;
-    end
+else  %training data and validation data
+    opts.validation = 1;
+    loss.val.e                 = [];
+    loss.val.e_frac            = [];
+end
+
+if ~isfield(opts,'plot')
+    fhandle = [];
+elseif opts.plot == 1
+    fhandle = figure();
+else
+    fhandle = [];
+end
+
+
+m = size(train_x, 1);
+
+if nn.normalize_input==1
+    [train_x, mu, sigma] = zscore(train_x);
+    nn.normalizeMean = mu;
+    sigma(sigma==0) = 0.0001;%this should be very small value.
+    nn.normalizeStd  = sigma;
+end
+
+
+batchsize = opts.batchsize;
+numepochs = opts.numepochs;
+
+numbatches = m / batchsize;
+
+assert(rem(numbatches, 1) == 0, 'numbatches must be a integer');
+
+L = zeros(numepochs*numbatches,1);
+n = 1;
+for i = 1 : numepochs
+    tic;
     
-    batchsize = opts.batchsize;
-    numepochs = opts.numepochs;
-
-    numbatches = m / batchsize;
-
-    assert(rem(numbatches, 1) == 0, 'numbatches must be a integer');
-
-    L = zeros(numepochs*numbatches,1);
-    n = 1;
-    for i = 1 : numepochs
-        tic;
-
-        kk = randperm(m);
-        for l = 1 : numbatches
-            batch_x = x(kk((l - 1) * batchsize + 1 : l * batchsize), :);
-            
-            %Add noise to input (for use in denoising autoencoder)
-            if(nn.inputZeroMaskedFraction ~= 0)
-                batch_x = batch_x.*(rand(size(batch_x))>nn.inputZeroMaskedFraction);
-            end
-            
-            batch_y = y(kk((l - 1) * batchsize + 1 : l * batchsize), :);
-            
-            nn = nnff(nn, batch_x, batch_y);
-            nn = nnbp(nn);            
-            nn = nnapplygrads(nn);
-
-            L(n) = nn.L;
-            
-            n = n + 1;
+    kk = randperm(m);
+    for l = 1 : numbatches
+        batch_x = train_x(kk((l - 1) * batchsize + 1 : l * batchsize), :);
+        
+        %Add noise to input (for use in denoising autoencoder)
+        if(nn.inputZeroMaskedFraction ~= 0)
+            batch_x = batch_x.*(rand(size(batch_x))>nn.inputZeroMaskedFraction);
         end
-
-        t = toc;
         
-        disp(['epoch ' num2str(i) '/' num2str(opts.numepochs) '. Took ' num2str(t) ' seconds' '. Mean squared error on training set is ' num2str(mean(L((n-numbatches):(n-1))))]);
+        batch_y = train_y(kk((l - 1) * batchsize + 1 : l * batchsize), :);
         
+        nn = nnff(nn, batch_x, batch_y);
+        nn = nnbp(nn);
+        nn = nnapplygrads(nn);
+        
+        L(n) = nn.L;
+        
+        n = n + 1;
     end
+    
+    t = toc;
+    
+    if ishandle(fhandle)
+        
+        if opts.validation == 1
+            loss = nneval(nn,loss,train_x,train_y,val_x,val_y);
+        else
+            loss = nneval(nn,loss,train_x,train_y);
+        end
+        
+        nnupdatefigures(nn,fhandle,loss,opts,i);
+    end
+        
+    disp(['epoch ' num2str(i) '/' num2str(opts.numepochs) '. Took ' num2str(t) ' seconds' '. Mean squared error on training set is ' num2str(mean(L((n-numbatches):(n-1))))]);
+    
+end
 end
 
--- tests/test_example_NN.m
+++ tests/test_example_NN.m
@@ -66,3 +66,47 @@ nn = nntrain(nn, train_x, train_y, opts);
 
 [er, bad] = nntest(nn, test_x, test_y);
 assert(er < 0.1, 'Too big error');
+
+%% ex5 plotting functionality
+rng(0);
+nn = nnsetup([784 20 10]);
+nn.normalize_input     = 1; 
+nn.activation_function = 'tanh_opt';        %  Sigmoid activation function
+nn.normalize_input     = 1;            
+nn.learningRate        = 0.1;          
+opts.numepochs         = 20;            %  Number of full sweeps through data
+opts.batchsize         = 100;           %  Take a mean gradient step over this many samples
+opts.plot              = 1;             %  enable plotting
+
+nn = nntrain(nn, train_x, train_y, opts);
+
+[er, bad] = nntest(nn, test_x, test_y);
+assert(er < 0.1, 'Too big error');
+
+%% ex6 neural net with sigmoid and plotting of validation and training error
+
+%create validation set
+load mnist_uint8;
+
+val_x   = double(train_x(1:10000,:)) / 255;
+train_x = double(train_x(10001:end,:)) / 255;
+test_x  = double(test_x)/255;
+
+val_y   = double(train_y(1:10000,:));
+train_y = double(train_y(10001:end,:));
+test_y  = double(test_y);
+
+
+rng(0);
+nn                      = nnsetup([784 20 10]);
+nn.normalize_input      = 0;
+nn.activation_function  = 'sigm';
+nn.output               = 'softmax';
+nn.learningRate         = 0.1;
+opts.numepochs          = 20;        %  Number of full sweeps through data
+opts.batchsize          = 100;       %  Take a mean gradient step over this many samples
+opts.plot               = 1;          %  enable plotting
+nn = nntrain(nn, train_x, train_y, opts,val_x,val_y);
+
+[er, bad] = nntest(nn, test_x, test_y);
+assert(er < 0.06, 'Too big error');
