2e5007ccd60e84c20e24f759cbcf9a4c687cccf5
Marek Otahal <markotahal@gmail.com>
2013-03-16 23:22:03 +0100

    nnsetup: use more neutral values
    =not optimal, but many other specific NN call this as internal Fn

--- util/nnsetup.m
+++ util/nnsetup.m
@@ -8,18 +8,18 @@ function nn = nnsetup(nn)
     nn.output                           = 'sigm';       % output unit 'sigm' (=logistic), 'softmax' and 'linear'
     nn.batchsize                        = 100;          % batchsize used. 1=online learning, size(data)=offline l., some value (100, 1000,...)=batch learning
     nn.numepochs                        = 5;            % how many sweeps through all dataset
-    nn.plot                             = 1;            % display training progress
+    nn.plot                             = 0;            % display training progress
 % learning (convergence speed)
     nn.activation_function              = 'tanh_opt';   %  Activation functions of hidden layers: 'sigm' (sigmoid) or 'tanh_opt' (optimal tanh).
-    nn.alpha                     = 2;            %  learning rate Note: typically needs to be lower when using 'sigm' activation function and non-normalized inputs.
-    nn.momentum                         = 0.2;          %  Momentum, faster conv, better stability, rec. values 0~0.3
-    nn.scaling_learningRate             = 0.99;         %  Scaling factor for the learning rate (each epoch), l.r. decline, (0.99^100=0.36)
+    nn.alpha                            = 2;            %  learning rate Note: typically needs to be lower when using 'sigm' activation function and non-normalized inputs.
+    nn.momentum                         = 0;            %  Momentum, faster conv, better stability, rec. values 0~0.3
+    nn.scaling_learningRate             = 1;         %  Scaling factor for the learning rate (each epoch), l.r. decline, (0.99^100=0.36)
 % generalization (stability)  
-    nn.weightPenaltyL2                  = 10^-5;        %  L2 regularization, weight decay, rec.val 10^-4~10^-5
+    nn.weightPenaltyL2                  = 0;            %  L2 regularization, weight decay, rec.val 10^-4~10^-5
     nn.nonSparsityPenalty               = 0;            %  Non sparsity penalty
     nn.sparsityTarget                   = 0.05;         %  Sparsity target
     nn.inputZeroMaskedFraction          = 0;            %  Used for Denoising AutoEncoders
-    nn.dropout                          = 0.5;          %  Dropout level (http://www.cs.toronto.edu/~hinton/absps/dropout.pdf),0~-0.5, needs time to settle,percantage(0==off, 0.6==60% neur dropped out)
+    nn.dropout                          = 0;            %  Dropout level (http://www.cs.toronto.edu/~hinton/absps/dropout.pdf),0~-0.5, needs time to settle,percantage(0==off, 0.6==60% neur dropped out)
 % other
     %nn.type    specific NN class used {'cnn','ffnn','dbn','sae','cae',...}
     %nn.W{i}    weights for each layer
