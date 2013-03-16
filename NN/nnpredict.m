212c784f47b4d9301fe44d89b94b5c85ded8c1a4
Marek Otahal <markotahal@gmail.com>
2013-03-16 17:26:33 +0100

    nnpredict, nntest: add documentation

--- NN/nnpredict.m
+++ NN/nnpredict.m
@@ -1,8 +1,9 @@
 function labels = nnpredict(nn, x)
+%% NNPREDICT let net classify given data x
+% no learning on this stage
     nn.testing = 1;
     nn = nnff(nn, x, zeros(size(x,1), nn.size(end)));
     nn.testing = 0;
     
-    [~, i] = max(nn.a{end},[],2);
-    labels = i;
+    [~, labels] = max(nn.a{end},[],2);
 end
