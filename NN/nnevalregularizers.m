cc8d360571683418edbbecde831e3cc9de4b0bf9
Ananda Narayan <anandan@umich.edu>
2013-09-05 23:58:12 -0400

    printing current sparsity computed on a per-hidden-unit basis for further evaluating sparsity regularization

--- NN/nnevalregularizers.m
+++ NN/nnevalregularizers.m
@@ -12,16 +12,19 @@ unitcost_weightL2 = unitcost_weightL2 / (nn.n-1);
 
 cost_sparsity = 0;
 unitcost_sparsity = 0;
+curr_sparsity = 0;
 for i = 2 : nn.n-1
     num_hid = numel(nn.p{i});
     kl_div_aggregated = nn.sparsityTarget * sum(log(nn.sparsityTarget ./ nn.p{i})) + (1 - nn.sparsityTarget) * sum(log((1 - nn.sparsityTarget) ./ (1 - nn.p{i})));
     cost_sparsity = cost_sparsity + nn.nonSparsityPenalty * kl_div_aggregated;
     unitcost_sparsity = unitcost_sparsity + kl_div_aggregated / num_hid;
+    curr_sparsity = curr_sparsity + mean(nn.p{i});
 end
 unitcost_sparsity = unitcost_sparsity / (nn.n-2);
+curr_sparsity = curr_sparsity / (nn.n-2);
 
 costs = [cost_prediction cost_weightL2 cost_sparsity];
 total_cost = sum(costs);
 
-fprintf('\tPercentage contributions (Prediction error, L2 regularizer, sparsity regularizer): %.1f, %.1f, %.1f;\t', 100*costs/total_cost);
-fprintf('Unit costs of (L2, sparsity) regularizers: %f, %f\n', unitcost_weightL2, unitcost_sparsity);
+fprintf('\tPercentage contributions (Prediction error, L2 regularizer, sparsity regularizer): %.1f, %.1f, %.1f;\n', 100*costs/total_cost);
+fprintf('\tUnit costs of (L2, sparsity) regularizers: %f, %f;\t Current sparsity: %f\n', unitcost_weightL2, unitcost_sparsity, curr_sparsity);
