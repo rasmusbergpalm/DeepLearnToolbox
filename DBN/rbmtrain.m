ce3c261ab06e76a80ff2c32e6b6382379cd0181a
Max Görner <max.goerner@uni-rostock.de>
2013-10-15 16:58:11 +0200

    I added comments for some RBM parameters and a comment in tanh_opt. I further applied some changes to rbmtrain, which I consider as elementary:     - It is possible now to continue learning until the error doesn't change any further.     - It is possible now to adjust the number of gibbs sampling steps. I therefor introduced 2 new options:     - opts.threshold to specify the threshold, how much the error has to change to abort learning. Default is 0.01.     - opts.numsampleits to specify the number of gibbs sampling steps. Default is 1. Note that no changes have to be done in order to keep the old behaviour. I furthermore changed sigm and sigmrnd to use the builtin logistic_cdf function to benefit from potential optimisations of this function.

--- DBN/rbmtrain.m
+++ DBN/rbmtrain.m
@@ -5,32 +5,49 @@ function rbm = rbmtrain(rbm, x, opts)
     
     assert(rem(numbatches, 1) == 0, 'numbatches not integer');
 
-    for i = 1 : opts.numepochs
+    run_until_threshold = opts.numepochs < 1;
+    if(!isfield(opts,"threshold")) opts.threshold = 0.01; endif;
+    if(!isfield(opts,"numsampleits")) opts.numsampleits = 1; endif;
+    it = 0;
+    oldErr = 0; err = inf;
+    do
+        oldErr = err;
+        it++;
         kk = randperm(m);
         err = 0;
         for l = 1 : numbatches
             batch = x(kk((l - 1) * opts.batchsize + 1 : l * opts.batchsize), :);
             
-            v1 = batch;
-            h1 = sigmrnd(repmat(rbm.c', opts.batchsize, 1) + v1 * rbm.W');
-            v2 = sigmrnd(repmat(rbm.b', opts.batchsize, 1) + h1 * rbm.W);
-            h2 = sigmrnd(repmat(rbm.c', opts.batchsize, 1) + v2 * rbm.W');
+            v_begin = batch;
+            h_end = h_begin = sigmrnd(repmat(rbm.c', opts.batchsize, 1) + v_begin * rbm.W');
+            
+            for it = 1:opts.numsampleits
+                v_end = sigmrnd(repmat(rbm.b', opts.batchsize, 1) + h_end * rbm.W);
+                h_end = sigmrnd(repmat(rbm.c', opts.batchsize, 1) + v_end * rbm.W');
+            end
 
-            c1 = h1' * v1;
-            c2 = h2' * v2;
+            c1 = h_begin' * v_begin;
+            c2 = h_end' * v_end;
 
+            #Calculate the changes to the parameters. Store them in order to compute a momentum term.
             rbm.vW = rbm.momentum * rbm.vW + rbm.alpha * (c1 - c2)     / opts.batchsize;
-            rbm.vb = rbm.momentum * rbm.vb + rbm.alpha * sum(v1 - v2)' / opts.batchsize;
-            rbm.vc = rbm.momentum * rbm.vc + rbm.alpha * sum(h1 - h2)' / opts.batchsize;
+            rbm.vb = rbm.momentum * rbm.vb + rbm.alpha * sum(v_begin - v_end)' / opts.batchsize;
+            rbm.vc = rbm.momentum * rbm.vc + rbm.alpha * sum(h_begin - h_end)' / opts.batchsize;
 
             rbm.W = rbm.W + rbm.vW;
             rbm.b = rbm.b + rbm.vb;
             rbm.c = rbm.c + rbm.vc;
 
-            err = err + sum(sum((v1 - v2) .^ 2)) / opts.batchsize;
+            err = err + sum(sum((v_begin - v_end) .^ 2)) / opts.batchsize;
         end
         
-        disp(['epoch ' num2str(i) '/' num2str(opts.numepochs)  '. Average reconstruction error is: ' num2str(err / numbatches)]);
-        
-    end
+        err /= numbatches;
+        if(run_until_threshold)
+            disp(['epoch ' num2str(i) '. Average reconstruction error is: ' num2str(err)]);
+        else
+            disp(['epoch ' num2str(it) '/' num2str(opts.numepochs)  '. Average reconstruction error is: ' num2str(err)]);
+        endif
+    until( run_until_threshold && (oldErr - err < opts.threshold) #Condition for running to threshold
+                                  ||
+          !run_until_threshold && (it >= opts.numepochs)); #Condition for iterating
 end
--- util/sigmrnd.m
+++ util/sigmrnd.m
@@ -1,4 +1,5 @@
 function X = sigmrnd(P)
 %     X = double(1./(1+exp(-P)))+1*randn(size(P));
-    X = double(1./(1+exp(-P)) > rand(size(P)));
+%   X = double(1./(1+exp(-P)) > rand(size(P)));
+    X = double(logistic_cdf(P) > rand(size(P)));
 end
