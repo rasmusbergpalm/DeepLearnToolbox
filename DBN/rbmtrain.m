0e10d99e41a4fed385728aa182baf55a1ad3c41e
Rasmus Berg Palm <rasmusbergpalm@gmail.com>
2013-10-18 17:23:50 +0200

    Revert "made some variable names self describing, corrected a typo"
    This reverts commit c5c322f360577add2f1efa9b353bad2936a9b612.

--- DBN/rbmtrain.m
+++ DBN/rbmtrain.m
@@ -5,51 +5,49 @@ function rbm = rbmtrain(rbm, x, opts)
     
     assert(rem(numbatches, 1) == 0, 'numbatches not integer');
 
-	#checking and initialising some options
-    runUntilThresholdIsReached = opts.numepochs < 1;
+    run_until_threshold = opts.numepochs < 1;
     if(!isfield(opts,"threshold")) opts.threshold = 0.01; endif;
-    if(!isfield(opts,"gibbsSamplingSteps")) opts.gibbsSamplingSteps = 1; endif;
-
-    iterator = 0;
-    oldError = 0; currentError = inf;
+    if(!isfield(opts,"numsampleits")) opts.numsampleits = 1; endif;
+    it = 0;
+    oldErr = 0; err = inf;
     do
-        oldError = currentError;
-        iterator++;
+        oldErr = err;
+        it++;
         kk = randperm(m);
-        currentError = 0;
+        err = 0;
         for l = 1 : numbatches
             batch = x(kk((l - 1) * opts.batchsize + 1 : l * opts.batchsize), :);
             
-            visibleActivationsAtBegin = batch;
-            hiddenActivationsAtEnd = hiddenActivationsAtBegin = sigmrnd(repmat(rbm.c', opts.batchsize, 1) + visibleActivationsAtBegin * rbm.W');
+            v_begin = batch;
+            h_end = h_begin = sigmrnd(repmat(rbm.c', opts.batchsize, 1) + v_begin * rbm.W');
             
-            for sampleStep = 1:opts.gibbsSamplingSteps
-                v_end = sigmrnd(repmat(rbm.b', opts.batchsize, 1) + hiddenActivationsAtEnd * rbm.W);
-                hiddenActivationsAtEnd = sigmrnd(repmat(rbm.c', opts.batchsize, 1) + v_end * rbm.W');
+            for it = 1:opts.numsampleits
+                v_end = sigmrnd(repmat(rbm.b', opts.batchsize, 1) + h_end * rbm.W);
+                h_end = sigmrnd(repmat(rbm.c', opts.batchsize, 1) + v_end * rbm.W');
             end
 
-            c1 = hiddenActivationsAtBegin' * visibleActivationsAtBegin;
-            c2 = hiddenActivationsAtEnd' * v_end;
+            c1 = h_begin' * v_begin;
+            c2 = h_end' * v_end;
 
             #Calculate the changes to the parameters. Store them in order to compute a momentum term.
             rbm.vW = rbm.momentum * rbm.vW + rbm.alpha * (c1 - c2)     / opts.batchsize;
-            rbm.vb = rbm.momentum * rbm.vb + rbm.alpha * sum(visibleActivationsAtBegin - v_end)' / opts.batchsize;
-            rbm.vc = rbm.momentum * rbm.vc + rbm.alpha * sum(hiddenActivationsAtBegin - hiddenActivationsAtEnd)' / opts.batchsize;
+            rbm.vb = rbm.momentum * rbm.vb + rbm.alpha * sum(v_begin - v_end)' / opts.batchsize;
+            rbm.vc = rbm.momentum * rbm.vc + rbm.alpha * sum(h_begin - h_end)' / opts.batchsize;
 
             rbm.W = rbm.W + rbm.vW;
             rbm.b = rbm.b + rbm.vb;
             rbm.c = rbm.c + rbm.vc;
 
-            currentError = currentError + sum(sum((visibleActivationsAtBegin - v_end) .^ 2)) / opts.batchsize;
+            err = err + sum(sum((v_begin - v_end) .^ 2)) / opts.batchsize;
         end
         
-        currentError /= numbatches;
-        if(runUntilThresholdIsReached)
-            disp(['epoch ' num2str(iterator) '. Average reconstruction currentErroror is: ' num2str(currentError)]);
+        err /= numbatches;
+        if(run_until_threshold)
+            disp(['epoch ' num2str(i) '. Average reconstruction error is: ' num2str(err)]);
         else
-            disp(['epoch ' num2str(iterator) '/' num2str(opts.numepochs)  '. Average reconstruction currentErroror is: ' num2str(currentError)]);
+            disp(['epoch ' num2str(it) '/' num2str(opts.numepochs)  '. Average reconstruction error is: ' num2str(err)]);
         endif
-    until( runUntilThresholdIsReached && (oldError - currentError < opts.threshold) #Condition for running to threshold
+    until( run_until_threshold && (oldErr - err < opts.threshold) #Condition for running to threshold
                                   ||
-          !runUntilThresholdIsReached && (iterator >= opts.numepochs)); #Condition for iterating
+          !run_until_threshold && (it >= opts.numepochs)); #Condition for iterating
 end
