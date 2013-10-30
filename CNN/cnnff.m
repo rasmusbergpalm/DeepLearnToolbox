3317980f331d9e4856a8a9ccb957b8fcdea06a67
sdemyanov <sdemyanov@users.noreply.github.com>
2013-10-31 02:07:29 +1100

    Update cnnff.m
    Max-pooling instead of average-pooling, arbitrary object and scaling sizes

--- CNN/cnnff.m
+++ CNN/cnnff.m
@@ -1,39 +1,45 @@
-function net = cnnff(net, x)
-    n = numel(net.layers);
-    net.layers{1}.a{1} = x;
-    inputmaps = 1;
+function [net, err] = cnnff(net, x)
+  n = numel(net.layers);
+  batchsize = size(x, 3); % number of examples in the minibatch
+  net.layers{1}.a{1} = x;
 
-    for l = 2 : n   %  for each layer
-        if strcmp(net.layers{l}.type, 'c')
-            %  !!below can probably be handled by insane matrix operations
-            for j = 1 : net.layers{l}.outputmaps   %  for each output map
-                %  create temp output map
-                z = zeros(size(net.layers{l - 1}.a{1}) - [net.layers{l}.kernelsize - 1 net.layers{l}.kernelsize - 1 0]);
-                for i = 1 : inputmaps   %  for each input map
-                    %  convolve with corresponding kernel and add to temp output map
-                    z = z + convn(net.layers{l - 1}.a{i}, net.layers{l}.k{i}{j}, 'valid');
-                end
-                %  add bias, pass through nonlinearity
-                net.layers{l}.a{j} = sigm(z + net.layers{l}.b{j});
-            end
-            %  set number of input maps to this layers number of outputmaps
-            inputmaps = net.layers{l}.outputmaps;
-        elseif strcmp(net.layers{l}.type, 's')
-            %  downsample
-            for j = 1 : inputmaps
-                z = convn(net.layers{l - 1}.a{j}, ones(net.layers{l}.scale) / (net.layers{l}.scale ^ 2), 'valid');   %  !! replace with variable
-                net.layers{l}.a{j} = z(1 : net.layers{l}.scale : end, 1 : net.layers{l}.scale : end, :);
-            end
-        end
+  for l = 2 : n   %  for each layer
+
+    if strcmp(net.layers{l}.type, 'c')
+      for j = 1 : net.layers{l}.outputmaps   %  for each output map
+        %  create temp output map
+        mapsize = net.layers{l-1}.mapsize;
+        z = zeros([mapsize batchsize] - [net.layers{l}.kernelsize(1) - 1 net.layers{l}.kernelsize(2) - 1 0]);
+        for i = 1 : net.layers{l-1}.outputmaps   %  for each input map
+          %  convolve with corresponding kernel and add to temp output map
+          z = z + convn(net.layers{l-1}.a{i}, net.layers{l}.k{i, j}, 'valid');              
+        end            
+        net.layers{l}.a{j} = sigm(z + net.layers{l}.b{j});          
+      end        
+
+    elseif strcmp(net.layers{l}.type, 's')
+      % downsample
+      s = net.layers{l}.scale;       
+      b = strel('rectangle', [s(1) s(2)]);
+      for j = 1 : net.layers{l-1}.outputmaps
+        fi = ceil((s+1)/2);
+        a = zeros(fi(1)+s(1)*(net.layers{l}.mapsize(1)-1), fi(2)+s(2)*(net.layers{l}.mapsize(2)-1), batchsize);
+        a(1:net.layers{l-1}.mapsize(1), 1:net.layers{l-1}.mapsize(2), :) = net.layers{l-1}.a{j};
+        z = imdilate(a, b);
+        net.layers{l}.a{j} = z(fi(1):s(1):end, fi(2):s(2):end, :);          
+      end
     end
+  end
 
-    %  concatenate all end layer feature maps into vector
-    net.fv = [];
-    for j = 1 : numel(net.layers{n}.a)
-        sa = size(net.layers{n}.a{j});
-        net.fv = [net.fv; reshape(net.layers{n}.a{j}, sa(1) * sa(2), sa(3))];
-    end
-    %  feedforward into output perceptrons
-    net.o = sigm(net.ffW * net.fv + repmat(net.ffb, 1, size(net.fv, 2)));
+  %  concatenate all end layer feature maps into vector
+  mapsize = net.layers{n}.mapsize;
+  maplen = mapsize(1) * mapsize(2);    
+  mapnum = net.layers{n}.outputmaps;
+  net.fv = zeros(maplen*mapnum, batchsize);
+  for j = 1 : mapnum      
+    net.fv((j-1)*maplen+1 : j*maplen, :) = reshape(net.layers{n}.a{j}, maplen, batchsize);
+  end
 
+  %  feedforward into output perceptrons
+  net.o = sigm(net.ffW * net.fv + repmat(net.ffb, 1, batchsize))';    
 end
