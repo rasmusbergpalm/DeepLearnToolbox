f810cc5fb38f7e689832352074168f31f5ada2d0
sdemyanov <sdemyanov@users.noreply.github.com>
2013-10-31 02:03:10 +1100

    Update cnnapplygrads.m
    Parameters are inside the net

--- CNN/cnnapplygrads.m
+++ CNN/cnnapplygrads.m
@@ -1,15 +1,15 @@
-function net = cnnapplygrads(net, opts)
-    for l = 2 : numel(net.layers)
-        if strcmp(net.layers{l}.type, 'c')
-            for j = 1 : numel(net.layers{l}.a)
-                for ii = 1 : numel(net.layers{l - 1}.a)
-                    net.layers{l}.k{ii}{j} = net.layers{l}.k{ii}{j} - opts.alpha * net.layers{l}.dk{ii}{j};
-                end
-                net.layers{l}.b{j} = net.layers{l}.b{j} - opts.alpha * net.layers{l}.db{j};
-            end
+function net = cnnapplygrads(net)
+  for l = 2 : numel(net.layers)
+    if strcmp(net.layers{l}.type, 'c')
+      for j = 1 : net.layers{l}.outputmaps
+        for i = 1 : net.layers{l-1}.outputmaps
+          net.layers{l}.k{i, j} = net.layers{l}.k{i, j} - net.alpha * net.layers{l}.dk{i, j};
         end
+        net.layers{l}.b{j} = net.layers{l}.b{j} - net.alpha * net.layers{l}.db{j};
+      end      
     end
+  end
 
-    net.ffW = net.ffW - opts.alpha * net.dffW;
-    net.ffb = net.ffb - opts.alpha * net.dffb;
+  net.ffW = net.ffW - net.alpha * net.dffW;
+  net.ffb = net.ffb - net.alpha * net.dffb;
 end
