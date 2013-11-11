5a34e8a7c5a3718ecbe74839e7487c0226a2048a
Rasmus Berg Palm <rasmusbergpalm@gmail.com>
2013-11-10 22:52:14 -0800

    fix tests, fail early for CNNs

--- tests/test_example_SAE.m
+++ tests/test_example_SAE.m
@@ -30,36 +30,3 @@ opts.batchsize = 100;
 nn = nntrain(nn, train_x, train_y, opts);
 [er, bad] = nntest(nn, test_x, test_y);
 assert(er < 0.16, 'Too big error');
-
-%% ex2 train a 100-100 hidden unit SDAE and use it to initialize a FFNN
-%  Setup and train a stacked denoising autoencoder (SDAE)
-rand('state',0)
-sae = saesetup([784 100 100]);
-sae.ae{1}.activation_function       = 'sigm';
-sae.ae{1}.learningRate              = 1;
-sae.ae{1}.inputZeroMaskedFraction   = 0.5;
-
-sae.ae{2}.activation_function       = 'sigm';
-sae.ae{2}.learningRate              = 1;
-sae.ae{2}.inputZeroMaskedFraction   = 0.5;
-
-opts.numepochs =   1;
-opts.batchsize = 100;
-sae = saetrain(sae, train_x, opts);
-visualize(sae.ae{1}.W{1}(:,2:end)')
-
-% Use the SDAE to initialize a FFNN
-nn = nnsetup([784 100 100 10]);
-nn.activation_function              = 'sigm';
-nn.learningRate                     = 1;
-
-%add pretrained weights
-nn.W{1} = sae.ae{1}.W{1};
-nn.W{2} = sae.ae{2}.W{1};
-
-% Train the FFNN
-opts.numepochs =   1;
-opts.batchsize = 100;
-nn = nntrain(nn, train_x, train_y, opts);
-[er, bad] = nntest(nn, test_x, test_y);
-assert(er < 0.1, 'Too big error');
