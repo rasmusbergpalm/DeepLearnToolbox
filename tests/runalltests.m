a6499c3d7527f78311fd777296c6f3c6aa70c5f9
Rasmus Berg Palm <rasmusbergpalm@gmail.com>
2013-11-12 18:50:13 +0100

    run tests

--- tests/runalltests.m
+++ tests/runalltests.m
@@ -1,7 +1,7 @@
 clear all; close all; clc;
 
-addpath(genpath('..'));
-dirlist = dir('./test_*');
+addpath(genpath('.'));
+dirlist = dir('tests/test_*');
 for i = 1:length(dirlist)
     name = dirlist(i).name(1:end-2);
     feval(name)
