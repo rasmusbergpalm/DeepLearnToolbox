5aacb2805bd8c23ff753c47cde02b9e115c876b1
Rasmus Berg Palm <rasmusbergpalm@gmail.com>
2013-11-30 13:37:20 +0100

    Update convn.m

--- CNN/convn.m
+++ CNN/convn.m
@@ -1,6 +1,7 @@
 %Overwrite convn function to handle octaves convn(a,b,'valid') bug. See http://savannah.gnu.org/bugs/?39314
 function result = convn(A, B, shape)
     if(isOctave() && strcmp(shape, 'valid'))
+    	disp('in here');
         m = size(A, 1) - size(B, 1) + 1;
         result = zeros(m, m);
         B = flipdim(B,3);
