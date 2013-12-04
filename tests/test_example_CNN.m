1f7e7abde07cbfa32e6ef3760cbbc94aa24a3bd3
rmanor <ran.manor@gmail.com>
2013-12-04 11:45:10 +0200

    Support for non-square kernels. Added kernel_x and kernel_y variable to convolution layer with backward compatibility for kernelsize.

--- tests/test_example_CNN.m
+++ tests/test_example_CNN.m
@@ -9,25 +9,27 @@ test_y = double(test_y');
 %% ex1 Train a 6c-2s-12c-2s Convolutional neural network 
 %will run 1 epoch in about 200 second and get around 11% error. 
 %With 100 epochs you'll get around 1.2% error
-rng(0)
+
+rand('state',0)
+
 cnn.layers = {
     struct('type', 'i') %input layer
-    struct('type', 'c', 'outputmaps', 6, 'kernelsize', 5) %convolution layer
+    struct('type', 'c', 'outputmaps', 6, 'kernel_y', 5, 'kernel_x', 5) %convolution layer
     struct('type', 's', 'scale', 2) %sub sampling layer
     struct('type', 'c', 'outputmaps', 12, 'kernelsize', 5) %convolution layer
     struct('type', 's', 'scale', 2) %subsampling layer
 };
-cnn = cnnsetup(cnn, train_x, train_y);
+
 
 opts.alpha = 1;
-opts.batchsize = 50;
-opts.numepochs = 1;
+opts.batchsize = 200;
+opts.numepochs = 7;
 
+cnn = cnnsetup(cnn, train_x, train_y);
 cnn = cnntrain(cnn, train_x, train_y, opts);
 
 [er, bad] = cnntest(cnn, test_x, test_y);
 
 %plot mean squared error
 figure; plot(cnn.rL);
-
 assert(er<0.12, 'Too big error');
