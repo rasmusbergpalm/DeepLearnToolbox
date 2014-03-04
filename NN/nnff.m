47ebe39054c05994ec426df9148a62a61d824ff8
Eric V. Strobl <ericvonstrobl@gmail.com>
2014-03-04 16:53:47 -0500

    Update nnff.m
    rectified linear units and softplus

--- NN/nnff.m
+++ NN/nnff.m
@@ -17,6 +17,10 @@ function nn = nnff(nn, x, y)
                 nn.a{i} = sigm(nn.a{i - 1} * nn.W{i - 1}');
             case 'tanh_opt'
                 nn.a{i} = tanh_opt(nn.a{i - 1} * nn.W{i - 1}');
+            case 'relu'
+                nn.a{i} = max(0,nn.a{i - 1} * nn.W{i - 1}');
+            case 'softp'
+                nn.a{i} = log(1+exp(nn.a{i - 1} * nn.W{i - 1}'));
         end
         
         %dropout
