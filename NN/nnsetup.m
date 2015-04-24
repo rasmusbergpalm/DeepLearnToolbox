4758895ed596d4091a50a4323263ddc39447d012
Tang RongKuan <ustjtrk@163.com>
2015-04-24 10:59:43 +0800

    add relu in nnsetup

--- NN/nnsetup.m
+++ NN/nnsetup.m
@@ -6,7 +6,8 @@ function nn = nnsetup(architecture)
     nn.size   = architecture;
     nn.n      = numel(nn.size);
     
-    nn.activation_function              = 'tanh_opt';   %  Activation functions of hidden layers: 'sigm' (sigmoid) or 'tanh_opt' (optimal tanh).
+    nn.activation_function              = 'tanh_opt';   %  Activation functions of hidden layers: 'sigm' (sigmoid) or 'tanh_opt' (optimal tanh) or 'relu'.
+    nn.neg_slope                        = 0;            % unit for relu
     nn.learningRate                     = 2;            %  learning rate Note: typically needs to be lower when using 'sigm' activation function and non-normalized inputs.
     nn.momentum                         = 0.5;          %  Momentum
     nn.scaling_learningRate             = 1;            %  Scaling factor for the learning rate (each epoch)
